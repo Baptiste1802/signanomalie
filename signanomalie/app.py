86081035e59488a50669242f1e6345b237619a85
baptiste <blambert1802@gmail.com>
2022-03-14 14:21:19 +0100

    rétablissement param api

--- signanomalie/app.py
+++ signanomalie/app.py
@@ -18,11 +18,11 @@ def mkpath(p):
 print(os.environ.get("URL"), os.environ.get("APP_TOKEN"), os.environ.get("API_TOKEN") )
 
 try:
-    # glpi = GLPI_CLIENT(
-    #     url = os.environ.get("URL"),
-    #     apptoken = os.environ.get("APP_TOKEN"),
-    #     auth = os.environ.get("API_TOKEN")
-    # )
-    glpi = None
+    glpi = GLPI_CLIENT(
+        url = os.environ.get("URL"),
+        apptoken = os.environ.get("APP_TOKEN"),
+        auth = os.environ.get("API_TOKEN")
+    )
+    # glpi = None
 except GLPIError as err:
     print("Error", str(err))
