e00e30bf6b2cd72cc8b5b7ae9dff053ed775868e
Jeremy Sanna <jeremysanna49@gmail.com>
2022-03-15 09:03:21 +0100

    Création page qrcode.html, ajout de la route /qrcode afin d'accéder à un formulaire de génération de qrcode

--- signanomalie/views.py
+++ signanomalie/views.py
@@ -23,4 +23,24 @@ def home():
         # locations = glpi.get_locations()
     
     )
+
+
+@app.route("/qrcode/")
+def qrcode():
+    form=SignalForm()
+    choicesbat = form.batiment.choices = ["grrrr"]
+    choicesSalleInfo = form.salle.choices = ["I01","I02","I03","I04","I05","I06","I07"]
+    choicesMateriel = form.materiel.choices = ["Clavier","Souris","Ecran","Unité centrale","Vidéo Projecteur"]
+
+    return render_template(
+        "qrcode.html",
+        title = "SignAnomalie",
+        form=form,
+        choicesbat=choicesbat,
+        choicesSalleInfo=choicesSalleInfo,
+        choicesMateriel=choicesMateriel,
+        # locations = glpi.get_locations()
+    
+    )
+
 app.config['SECRET_KEY'] = "f2ef2834-7d1c-47a4-b35c-f48547b6c6dc"
