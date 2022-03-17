73ff7ba9c3048b6519444a11c9b2f90113ba4f75
baptiste <blambert1802@gmail.com>
2022-03-17 19:59:06 +0100

    ajout des routes pour récupérer les printers et monitors

--- signanomalie/api_rest.py
+++ signanomalie/api_rest.py
@@ -11,3 +11,13 @@ def api_get_locations(id):
 def api_get_computers(id):
     '''return sub items of a location'''
     return jsonify(glpi.get_computers_per_location(id))
+
+@app.route("/api/get/printers/<int:id>", methods=["GET", "POST"])
+def api_get_printers(id):
+    '''return sub items of a location'''
+    return jsonify(glpi.get_printers_per_location(id))
+
+@app.route("/api/get/monitors/<int:id>", methods=["GET", "POST"])
+def api_get_monitors(id):
+    '''return sub items of a location'''
+    return jsonify(glpi.get_monitors_per_location(id))
